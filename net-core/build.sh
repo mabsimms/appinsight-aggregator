e8a76a75f8d8b6bf59186b2b651b20d8e682ad1d
Mark Simms <masimms@microsoft.com>
2016-08-23 13:47:37 -0700

    Cleaning up old directories

--- net-core/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t masimms/appinsight-aggregator-demo:$1 .
-docker save masimms/appinsight-aggregator-demo:$1 > appinsight-aggregator-demo.tar
--- net-core/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d --net=host \
-    --ulimit nofile=40012:40012 \
-    --name appinsight-demo-graphite \
-    masimms/appinsight-aggregator-demo:$1 \
