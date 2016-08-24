27275fd717abd10e10ba7255048865b6ced8ebaa
Mark Simms <masimms@microsoft.com>
2016-08-23 19:34:42 -0700

    Cleaning up naming

--- Microsoft.AzureCAT.Extensions.AppInsight/src/Microsoft.AzureCAT.Extensions.AppInsight/Utils/OpenSchemaCallbackClient.cs
+++ Microsoft.AzureCAT.Extensions.AppInsight/src/Microsoft.AzureCAT.Extensions.AppInsight/Utils/OpenSchemaCallbackClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AzureCAT.Extensions.AppInsight.Utils.Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json.Linq;
@@ -17,37 +18,43 @@ namespace Microsoft.AzureCAT.Extensions.AppInsight.Utils
             string schemaName,
             string iKey)
         {
-            var sasPolicy = new SharedAccessBlobPolicy();
-            sasPolicy.SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24);
-            sasPolicy.Permissions = SharedAccessBlobPermissions.Read;            
-            var sasToken = blob.GetSharedAccessSignature(sasPolicy);
-            var sasUri = blob.Uri + sasToken;
+            try
+            {
+                var sasPolicy = new SharedAccessBlobPolicy();
+                sasPolicy.SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24);
+                sasPolicy.Permissions = SharedAccessBlobPermissions.Read;            
+                var sasToken = blob.GetSharedAccessSignature(sasPolicy);
+                var sasUri = blob.Uri + sasToken;
           
-            var payload = new JObject(
-                new JProperty("data",
-                    new JObject(
-                        new JProperty("baseType", "OpenSchemaData"),
-                        new JProperty("baseData", new JObject(
-                            new JProperty("ver", "2"),
-                            new JProperty("blobSasUri", sasUri),
-                            new JProperty("sourceName", schemaName),
-                            new JProperty("sourceVersion", "1.0")
+                var payload = new JObject(
+                    new JProperty("data",
+                        new JObject(
+                            new JProperty("baseType", "OpenSchemaData"),
+                            new JProperty("baseData", new JObject(
+                                new JProperty("ver", "2"),
+                                new JProperty("blobSasUri", sasUri),
+                                new JProperty("sourceName", schemaName),
+                                new JProperty("sourceVersion", "1.0")
+                                )
                             )
                         )
-                    )
-                ),
-                new JProperty("ver", "1"),
-                new JProperty("name", "Microsoft.ApplicationInsights.OpenSchema"),
-                new JProperty("time", DateTime.UtcNow),
-                new JProperty("iKey", iKey)
-            );
+                    ),
+                    new JProperty("ver", "1"),
+                    new JProperty("name", "Microsoft.ApplicationInsights.OpenSchema"),
+                    new JProperty("time", DateTime.UtcNow),
+                    new JProperty("iKey", iKey)
+                );
 
-            // TODO; pool the http client
-            var httpClient = new HttpClient();
-            await httpClient
-                .PostAsync(endpoint, new StringContent(payload.ToString()))
-                .ConfigureAwait(false);
-
+                // TODO; pool the http client
+                var httpClient = new HttpClient();
+                await httpClient
+                    .PostAsync(endpoint, new StringContent(payload.ToString()))
+                    .ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                CoreEventSource.Log.LogVerbose("Could not publish callback", ex.ToString());
+            }
         }
     }
 }
